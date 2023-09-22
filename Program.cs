f4e191cee4c1ae87aabee83fd8fe2a17ed3cd45d
Menaid <menaidnocic@live.se>
2023-09-22 10:54:22 +0200

    First init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿int[] numbers = { 12, 21, 3, 4, 0, 6, 1, 8, 9 };
+for (int i = 0; i < numbers.Length; i++)
+{
+    sort();
+}
+
+foreach (int num in numbers)
+{
+    Console.Write(num + ", ");
+}
+void sort()
+{
+for (int i = 0; i < numbers.Length -1; i++)
+{
+    int a = numbers[i];
+    int b = numbers[i + 1];
+
+    if (a > b)
+    {
+        numbers[i] = b;
+        numbers[i + 1] = b;
+    }
+}
+
+}
+
+void display()
+{
+    foreach (int a in numbers)
+    {
+
+    }
+}
